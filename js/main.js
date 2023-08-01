da3df4dca972f17a1f4294fcba1a808340e3828b
Alex <alexlysak2688@gmail.com>
2023-08-01 18:18:25 +0300

    add media 768,480 and 360 section

--- js/main.js
+++ js/main.js
@@ -7,8 +7,28 @@ menuBtn.addEventListener('click', () => {
 
 
 const swiperOne = new Swiper('.feedback__slider', {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
+   loop: true,
+   pagination: {
+      el: '.swiper-pagination',
+   },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+   loop: true,
+   slidesPerView: 3,
+   spaceBetween: 20,
+   pagination: {
+      el: '.swiper-pagination',
+   },
+   breakpoints: {
+      640: {
+         slidesPerView: 3,
+      },
+      480: {
+         slidesPerView: 2,
+      },
+      360: {
+         slidesPerView: 1,
+      },
+   },
 });
